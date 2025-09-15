0272d97c0da9e59b5585b7e05b51841326719395
Tim Purdum <tim.purdum@dymaptic.com>
2025-09-15 08:08:02 -0500

    wip

--- TimPurdum.Dev.BlogGenerator.Shared/AbstractTemplates/BaseFooter.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace TimPurdum.Dev.BlogGenerator.Shared.AbstractTemplates;
-
-public class BaseFooter: ComponentBase
-{
-    
-}
--- TimPurdum.Dev.BlogGenerator.Shared/LinkData.cs
+++
@@ -1,3 +0,0 @@
-namespace TimPurdum.Dev.BlogGenerator;
-
-public record LinkData(string Title, string SubTitle, string Url, DateTime PublishedDate, string Author);
